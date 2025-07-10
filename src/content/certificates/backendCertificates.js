e33b3cf5cde4bc2669d0592294f9fec70fef5152
ellaggon <ellaggon@gmail.com>
2025-07-10 10:48:19 -0400

    feat(content): create archives to save url and title of certificates

---
+++ src/content/certificates/backendCertificates.js
@@ -0,0 +1,13 @@
+export const backendCertificates = [
+  { src: "https://imgur.com/4vJDBsR.jpg", alt: "Fundamentos de Ingenieria de Software" },
+  { src: "https://i.imgur.com/iA9qkwI.jpg", alt: "Fundamentos de Node.js" },
+  { src: "https://i.imgur.com/VFfnLvT.jpg", alt: "Vite" },
+  { src: "https://i.imgur.com/sLrhs05.jpg", alt: "Backend" },
+  { src: "https://i.imgur.com/7F5Yl4y.jpg", alt: "Backend con Node.js y Express" },
+  { src: "https://i.imgur.com/lxzJvRG.jpg", alt: "Base de Datos" },
+  { src: "https://i.imgur.com/D7EZInh.jpg", alt: "Git y Github" },
+  { src: "https://i.imgur.com/K9IppfR.png", alt: "Javascript engine (V8) y el navegador" },
+  { src: "https://i.imgur.com/vO2R4FA.jpg", alt: "Consumo de API REST con Javascript - Teórico" },
+  { src: "https://i.imgur.com/8k46VN1.jpg", alt: "Consumo de API REST con Javascript - Práctico" },
+  { src: "https://imgur.com/O8s5HqT.jpg", alt: "SQL y MySQL" }
+]
---
+++ src/content/certificates/extraCertificates.js
@@ -0,0 +1,8 @@
+export const extraCertificates = [
+  { src: "https://i.imgur.com/2qYAa5N.png", alt: "Pre-works" },
+  { src: "https://i.imgur.com/JyUbo9J.png", alt: "Defi" },
+  { src: "https://i.imgur.com/LMVGrzQ.png", alt: "Finanzas Descentralizadas" },
+  { src: "https://i.imgur.com/NAlhR7i.png", alt: "Psicología del consumidor" },
+  { src: "https://i.imgur.com/Hp8ZG80.png", alt: "Business model canvas" },
+  { src: "https://i.imgur.com/2GLeXXK.jpg", alt: "Supervisor de Seguridad" }
+]
